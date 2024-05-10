004bff9731acb604638d2224be4af0c813240e84
Josh Stevenson <104766099+joshjstevenson@users.noreply.github.com>
2024-05-10 15:10:44 +0100

    Add files via upload

---
+++ train-information.js
@@ -0,0 +1,16 @@
+
+function callStation(location) {
+    const response = fetch(`https://api.tfl.gov.uk/Line/${location}/Arrivals`, {
+            method: 'GET',
+            // Request headers
+            headers: {
+                'Cache-Control': 'no-cache',}
+        })
+        .then(response => {
+            console.log(response.status);
+            console.log(response.text());
+        })
+        .catch(err => console.error(err));
+
+    return response;
+}
